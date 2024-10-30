4b11687bc1821304218d245675609779f55c75f3
patrickweglinski <113038385+patrickweglinski@users.noreply.github.com>
2024-10-30 15:49:16 -0600

    first commit

---
+++ RandomTest.py
@@ -0,0 +1,13 @@
+#Random Processes 10/30/24
+#By Patrick, Skyler, Jace
+from math import sqrt, exp, factorial
+import random as r
+from random import random
+import matplotlib.pyplot as plt
+
+
+i = random()
+print(f"this is working {round(i,2)}")
+
+
+
