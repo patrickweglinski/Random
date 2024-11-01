d0fd73477e0e5c10a50e70ea225c17efd9f7bf2c
patrickweglinski <113038385+patrickweglinski@users.noreply.github.com>
2024-11-01 15:03:57 -0600

    here ya go

--- RandomTest.py
+++ RandomTest.py
@@ -1,13 +1,36 @@
 #Random Processes 10/30/24
 #By Patrick, Skyler, Jace
-from math import sqrt, exp, factorial
+from math import sqrt, exp, factorial, floor
 import random as r
 from random import random
 import matplotlib.pyplot as plt
 
+# for i in range(20):
+#     print(random())
 
-i = random()
-print(f"this is working {round(i,2)}")
+# for i in range(20):
+#     print(random())
+
+# for i in range(50):
+#     print(int(10*random()))
+
+x = []
+y = []
+
+for i in range(1000):
+    x[i] = 10*random()
+
+for i in range(1000):
+    y[i] = 10*random()
+
+plt.plot(x, y,marker="o", markersize=2,color="red")
+plt.show()
+    
+
+
+
+
+
 
 
 
