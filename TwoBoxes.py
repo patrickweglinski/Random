28b37583c24dc6fd6382657cdbb9a7e33ece742f
patrickweglinski <113038385+patrickweglinski@users.noreply.github.com>
2024-11-01 15:33:21 -0600

    new program

---
+++ TwoBoxes.py
@@ -0,0 +1,17 @@
+#Random Processes 10/30/24
+#By Patrick, Skyler, Jace
+from math import sqrt, exp, factorial, floor
+import random as r
+from random import random
+import matplotlib.pyplot as plt
+
+
+    
+
+
+
+
+
+
+
+
