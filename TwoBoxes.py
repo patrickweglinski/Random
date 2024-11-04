7ea61c07bd20215544d378b484680f8923c9bd41
patrickweglinski <113038385+patrickweglinski@users.noreply.github.com>
2024-11-04 16:35:41 -0700

    monday

--- TwoBoxes.py
+++ TwoBoxes.py
@@ -6,11 +6,13 @@ from random import random
 import matplotlib.pyplot as plt
 
 #total number of molecules
-n = 1000
-t = 10000
+n = 100
+
+#number of timesteps
+t = 100000
 
 #molecules in box
-nLeft = n/2
+nLeft = n//2
 
 #lists for plotting
 tList = [0]*t
@@ -39,15 +41,55 @@ for i in range(t):
 # plt.ylabel("nleft amount")
 # plt.show()
 
-plt.bar(range(len(hist)),hist,)
-plt.title("Amount of time Nleft value occured")
-plt.xlim(425,575)
-plt.xlabel("value")
-plt.ylabel("Amount of value occurences")
-plt.show()
 
+min = 0
+max = 0
 
+for i in range(len(hist)):
     
+    if hist[i] > 0:
+        min = i-1
+        break
+
+for i in range(len(hist)):
+    
+    if hist[i] > 0:
+        max = i+1
+
+# plt.bar(range(len(hist)),hist)
+# plt.title("Number of events Nleft value occured")
+# plt.xlim(min,max)
+# plt.xlabel("value")
+# plt.ylabel("events")
+# plt.show()
+
+
+
+def probability(n,nLeft): 
+    return(1/2**n)*(factorial(n))/(factorial(nLeft)*(factorial(n-nLeft)))
+
+# for i in range(len(nLeftList)):
+#     probabilityList[i] = probability(nLeftList[i])
+
+probabilityList = [0]*n
+
+
+leftbucket = 0
+
+for i in range(n):
+    probabilityList[i] = t*probability(n, leftbucket)
+    leftbucket+=1
+
+
+
+plt.plot(range(len(probabilityList)),probabilityList,linestyle="solid", color="red")
+plt.bar(range(len(hist)),hist,label="Monte Carlo results")
+plt.title("Probabilty of events and number of events")
+# plt.xlim(min,max)
+plt.xlabel("value")
+plt.ylabel("events and probability of the event")
+plt.legend(["probability","events"])
+plt.show()
 
 
 
