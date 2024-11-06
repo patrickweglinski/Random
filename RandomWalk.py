fec2a5ab54471675b96a662e8925e721b5332f80
patrickweglinski <113038385+patrickweglinski@users.noreply.github.com>
2024-11-06 15:43:34 -0700

    here

---
+++ RandomWalk.py
@@ -0,0 +1,52 @@
+#RandomWalks 10/30/24
+#By Patrick, Skyler, Jace
+
+import random 
+import math 
+import matplotlib.pyplot as plt
+
+t = 1
+timesteps = 10000
+position = 0
+walkplot = [0]*timesteps
+legend = []
+distance = [0]*t
+
+# while t <= 1:
+
+walkplot[0] = position
+
+while t <= 20:
+
+    position = 0
+
+    for i in range(timesteps-1):
+        
+        if random.random() < .5:
+            position -= 1
+
+        else:
+            position += 1
+
+        walkplot[i+1] = position
+
+    legend.append(t)
+        
+    plt.plot(range(len(walkplot)),walkplot,marker="+", markersize=3)
+    plt.grid()
+    plt.title("Random Walk")
+    plt.xlabel("steps")
+    plt.ylabel("distance away from origin")
+    # plt.legend(legend)
+
+    t +=1
+
+plt.show()
+
+
+
+
+
+
+
+
